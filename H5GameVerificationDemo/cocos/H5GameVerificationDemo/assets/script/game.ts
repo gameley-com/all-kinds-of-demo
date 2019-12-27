86b981d58fbc39b6f1d9e78bfeca9da8a63f3c32
baozilaji <baozilaji@126.com>
2019-12-27 13:18:36 +0800

    接入CDB，实现乐游登录流程。

--- H5GameVerificationDemo/cocos/H5GameVerificationDemo/assets/script/game.ts
+++ H5GameVerificationDemo/cocos/H5GameVerificationDemo/assets/script/game.ts
@@ -1,34 +1,85 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const {ccclass, property} = cc._decorator;
+
+import global_data from './global_data'
+import {CDB} from './CDB'
 
 @ccclass
 export default class game extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property(cc.WebView)
+    webUrl: cc.WebView = null;
 
-    @property
-    text: string = 'hello';
+    @property(cc.Node)
+    loading: cc.Node = null;
 
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
+    getParam() {
+        let url = window.location.href
+        let index = url.indexOf("?");
+        let ret = {}
+        if (index != -1) {
+            url = url.substring(index + 1);
+            let params = url.split("&");
+            params.forEach(function (item) {
+                if (item && item.length > 0) {
+                    let kv = item.split("=");
+                    if (kv.length == 2 && kv[0] && kv[0].length > 0) {
+                        ret[kv[0]] = kv[1];
+                    }
+                }
+            });
+        }
+        return ret;
+    }
 
     start () {
-        console.log("game started....")
+        CDB.debugMode=true;
+        console.log("game started...."+window.location.href)
+        let that = this
+        if (!global_data.Instance.isLogin()) {//用户还未登录
+            console.log("user hasn't login yet.")
+            let param = that.getParam()
+            if (param['code']) {
+                that.loading.active=true
+                //webview授权登录成功，带着code返回游戏
+                //TODO 初始化CDB登录，完成登录流程
+                let params = {
+                    gameId: 2,
+                    channelId: 10001,
+                    platId: 0,
+                    code: param['code'],
+                    timestamps: Math.floor((new Date()).valueOf() / 1000).toString(),
+                    success: function(result)
+                    {
+                        console.log('CDB login Success 上行:' + JSON.stringify(params) + " 下行:" + JSON.stringify(result))
+                        global_data.Instance.login();
+                        cc.director.loadScene("running");
+                        that.loading.active=false;
+                    },
+                    fail: function(reason) {
+                        console.error('CDB login Fail 上行:' + JSON.stringify(params) + " 下行:" + JSON.stringify(reason))
+                    }
+                }
+                CDB.login(params);
+            }
+        }
     }
 
     startGame(){
-        cc.director.loadScene("running");
+        //玩家已经登录，直接开始游戏
+        if (global_data.Instance.isLogin()) {
+            console.log("user has logined in.")
+            cc.director.loadScene("running");
+        }else{
+            console.log("not login.");
+            //如果玩家未登录，并且用户来自手机H5页面，则跳出授权登录页面
+            if(cc.sys.platform === cc.sys.MOBILE_BROWSER||cc.sys.platform===cc.sys.DESKTOP_BROWSER){
+                console.log("show login page.")
+                this.webUrl.node.active=true
+            }else{
+                console.log("run game...")
+                cc.director.loadScene("running");
+            }
+        }
     }
 
     // update (dt) {}
---
+++ H5GameVerificationDemo/cocos/H5GameVerificationDemo/assets/script/global_data.ts
@@ -0,0 +1,23 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+export default class global_data {
+    public static readonly Instance:global_data = new global_data();
+    private _login: boolean = false;
+    constructor(){
+        console.log("new global_data construtor")
+    }
+    login(){
+        this._login = true;
+    } 
+    isLogin(){
+        return this._login;
+    }
+}
